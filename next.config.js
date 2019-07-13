0ce76d7cfe9138c5f75c8c2d1125a4b917ffb067
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2019-07-13 10:12:40 +0000

    Update Tetengo2

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ const path = require('path')
 const {
   NOTION_TOKEN,
   BLOG_INDEX_ID,
-} = require('./sr
+} = require('./src/lib/notion/server-constants')
+
+try {
+  fs.unlinkSync(path.resolve('.blog_index_data'))
+} catch (_) {
+  /* non fatal */
+}
+try {
+  fs.unlinkSync(path.resolve('.
