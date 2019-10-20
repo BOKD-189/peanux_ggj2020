77a464ae6cda347666d6f3aa3f3d8d5e60a918ab
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2019-10-20 11:42:53 +0000

    local config

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,1 +1,8 @@
-import Pr
+import Prism from 'prismjs'
+import 'prismjs/components/prism-jsx'
+
+const Code = ({ children, language = 'javascript' }) => {
+  return (
+    <>
+      <pre>
+        <c
