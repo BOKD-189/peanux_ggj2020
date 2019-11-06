1033fd44745f6c484e794b052dd441328238feb8
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2019-11-06 11:04:23 +0000

    Bump uglifier from 4.1.19 to 4.1.20

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,1 +1,7 @@
 import React, { useState } from 'react'
+
+const Counter = ({ initialValue }) => {
+  const [clicks, setClicks] = useState(initialValue)
+
+  return (
+    <div style={{ margin: '10px 0 20px' }}>
