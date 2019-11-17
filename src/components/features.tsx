09f8dabc316b9a04027f7678ba30d34bd1ec9f3e
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2019-11-17 17:44:55 +0000

    README typo.

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -3,4 +3,14 @@ import Jamstack from './svgs/jamstack'
 import Wifi from './svgs/wifi'
 import Lighthouse from './svgs/lighthouse'
 import Plus from './svgs/plus'
-import Notion f
+import Notion from './svgs/notion'
+import Edit from './svgs/edit'
+import Scroll from './svgs/scroll'
+
+const features = [
+  {
+    text: 'Blazing fast',
+    icon: Lightning,
+  },
+  {
+    t
