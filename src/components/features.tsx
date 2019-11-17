f7433d16b37142f88c9e3385b0c923c90a18ddf1
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2019-11-17 10:10:03 +0000

    some tidying up

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,1 +1,6 @@
-import Lightning from './svgs/lightning
+import Lightning from './svgs/lightning'
+import Jamstack from './svgs/jamstack'
+import Wifi from './svgs/wifi'
+import Lighthouse from './svgs/lighthouse'
+import Plus from './svgs/plus'
+import Notion f
