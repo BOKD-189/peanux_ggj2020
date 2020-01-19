0aac1eebd2cb2b17f1012a1d4f0374e0482a4c95
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-01-19 22:11:39 +0000

    x86: Eliminate irq_mis_count counted in arch_irq_stat

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,4 +1,10 @@
 const collectText = (el, acc = []) => {
   if (el) {
     if (typeof el === 'string') acc.push(el)
-    if (Array.isArray(el)) el.map((item) => collectText
+    if (Array.isArray(el)) el.map((item) => collectText(item, acc))
+    if (typeof el === 'object') collectText(el.props && el.props.children, acc)
+  }
+  return acc.join('').trim()
+}
+
+const Heading = ({ children: 
