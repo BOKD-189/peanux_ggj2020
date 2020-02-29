ca6837408b0ebae4dd94207f912a92fc774f2994
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-02-29 13:53:06 +0000

    version bump to 1.2.0

--- src/components/svgs/edit.tsx
+++ src/components/svgs/edit.tsx
@@ -13,4 +13,12 @@ const Edit = (props) => (
     <path d="M4 10V3" />
     <path d="M12 21v-9" />
     <path d="M12 8V3" />
-    <path d="M20 21
+    <path d="M20 21v-5" />
+    <path d="M20 12V3" />
+    <path d="M1 14h6" />
+    <path d="M9 8h6" />
+    <path d="M17 16h6" />
+  </svg>
+)
+
+export default Edit
