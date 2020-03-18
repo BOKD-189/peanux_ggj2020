eef59ae56056b2facf3cb3e051ed195a0ca6e7f2
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-03-18 07:15:52 +0000

    theme changes

--- src/components/svgs/lightning.tsx
+++ src/components/svgs/lightning.tsx
@@ -1,2 +1,10 @@
 const Lightning = (props) => (
   <svg
+    width="24"
+    height="24"
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="1.5"
+    shapeRendering=
