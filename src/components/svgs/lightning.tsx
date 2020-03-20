f359b52ff9e808eaa36ec60a40fea164775ffe08
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-03-20 18:24:26 +0000

    am: Use cat instead of echo to avoid DOS line-endings (fixes t4150)

--- src/components/svgs/lightning.tsx
+++ src/components/svgs/lightning.tsx
@@ -7,4 +7,12 @@ const Lightning = (props) => (
     strokeLinecap="round"
     strokeLinejoin="round"
     strokeWidth="1.5"
-    shapeRendering=
+    shapeRendering="geometricPrecision"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z"></path>
+  </svg>
+)
+
+export default Lightning
---
+++ src/components/svgs/linkedin.tsx
@@ -0,0 +1,1 @@
+const Linkedin = (props) =
