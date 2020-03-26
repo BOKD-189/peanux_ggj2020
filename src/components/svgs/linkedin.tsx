d021f55fe3d62ff64ed0994adb9ab0c6830f3f18
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-03-26 12:37:59 +0000

    corrected errors

--- src/components/svgs/linkedin.tsx
+++ src/components/svgs/linkedin.tsx
@@ -1,1 +1,3 @@
-const Linkedin = (props) =
+const Linkedin = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" {...props}>
+    <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 3
