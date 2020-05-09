3bcedbe24b0b6ff0f8f95099f9412613d961baaa
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-05-09 14:55:43 +0000

    Update sender.py

--- src/components/svgs/wifi.tsx
+++ src/components/svgs/wifi.tsx
@@ -3,4 +3,11 @@ const Wifi = (props) => (
     viewBox="0 0 24 24"
     stroke="currentColor"
     strokeWidth="1.5"
-    strokeLi
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    fill="none"
+    shapeRendering="geometricPrecision"
+    {...props}
+  >
+    <path d="M5 12.55a11 11 0 0114.08 0" />
+    <path d="M1.42
