be7743fb7398b7793a41b049c591ec2cd8f357b8
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-05-13 06:31:00 +0000

    haha

--- src/components/svgs/zeit.tsx
+++ src/components/svgs/zeit.tsx
@@ -1,1 +1,6 @@
-const Zeit
+const Zeit = (props) => (
+  <svg width={114} height={100} viewBox="0 0 114 100" {...props}>
+    <title>{'Logotype - Black'}</title>
+    <defs>
+      <linearGradient
+        
