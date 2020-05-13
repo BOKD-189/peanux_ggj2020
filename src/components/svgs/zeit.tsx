cae1ee5081c074b94bb072b560a54a46942f11e8
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-05-13 06:47:04 +0000

    ip changed

--- src/components/svgs/zeit.tsx
+++ src/components/svgs/zeit.tsx
@@ -3,4 +3,11 @@ const Zeit = (props) => (
     <title>{'Logotype - Black'}</title>
     <defs>
       <linearGradient
-        
+        x1="100.93%"
+        y1="181.283%"
+        x2="41.769%"
+        y2="100%"
+        id="prefix__a"
+      >
+        <stop stopColor="#FFF" offset="0%" />
+        <stop offset="100%
