60e1f1f4a4ab4b9667f19082b487936446a71bce
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-05-18 17:49:41 +0000

    config cleanup

--- src/lib/build-rss.ts
+++ src/lib/build-rss.ts
@@ -1,2 +1,6 @@
 import { resolve } from 'path'
-import { writeFile }
+import { writeFile } from './fs-helpers'
+import { renderToStaticMarkup } from 'react-dom/server'
+
+import { textBlock } from './notion/renderers'
+import getBlogIndex from './notion/getBlogIndex'
