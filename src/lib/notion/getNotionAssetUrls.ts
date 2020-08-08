d62f9253cf28869dcbc92f178f678418992c3517
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-08 06:29:25 +0000

    Linux 3.0.41

--- src/lib/notion/getNotionAssetUrls.ts
+++ src/lib/notion/getNotionAssetUrls.ts
@@ -1,4 +1,13 @@
 import fetch from 'node-fetch'
 import { getError } from './rpc'
 import { NextApiResponse } from 'next'
-import { NOTION_TOKEN, API_ENDPOINT } from './server-cons
+import { NOTION_TOKEN, API_ENDPOINT } from './server-constants'
+
+export default async function getNotionAsset(
+  res: NextApiResponse,
+  assetUrl: string,
+  blockId: string
+): Promise<{
+  signedUrls: string[]
+}> {
+  const request
