d3cc1899aa7656322ecf6af5f6fd14a4888bf0fc
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-11 15:12:36 +0000

    Updated REAME

--- src/lib/notion/getPageData.ts
+++ src/lib/notion/getPageData.ts
@@ -1,4 +1,10 @@
 import rpc, { values } from './rpc'
 
 export default async function getPageData(pageId: string) {
-  // a reasonable size limit for th
+  // a reasonable size limit for the largest blog post (1MB),
+  // as one chunk is about 10KB
+  const maximumChunckNumer = 100
+
+  try {
+    var chunkNumber = 0
+    var data = await loadPageChunk(
