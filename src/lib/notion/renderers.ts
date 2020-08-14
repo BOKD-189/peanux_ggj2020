aa598e6bc200c77d937ddbbc6386885e85094834
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-14 11:55:26 +0000

    Fix comma

--- src/lib/notion/renderers.ts
+++ src/lib/notion/renderers.ts
@@ -2,4 +2,10 @@ import React from 'react'
 import components from '../../components/dynamic'
 
 function applyTags(tags = [], children, noPTag = false, key) {
- 
+  let child = children
+
+  for (const tag of tags) {
+    const props: { [key: string]: any } = { key }
+    let tagName = tag[0]
+
+    if (noPTag && tagNa
