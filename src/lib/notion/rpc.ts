3c56bfaa7fe50f9d38935a4d2910be6174d8bf3a
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-15 13:21:53 +0000

    automated merge

--- src/lib/notion/rpc.ts
+++ src/lib/notion/rpc.ts
@@ -1,2 +1,6 @@
 import fetch, { Response } from 'node-fetch'
-import 
+import { API_ENDPOINT, NOTION_TOKEN } from './server-constants'
+
+export default async function rpc(fnName: string, body: any) {
+  if (!NOTION_TOKEN) {
+    t
