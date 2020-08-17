5a6e8c20bc29c26394522d3cefe6f499e06f08da
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-17 15:16:20 +0000

    Requested changes.

--- src/lib/notion/server-constants.js
+++ src/lib/notion/server-constants.js
@@ -1,2 +1,8 @@
 // use commonjs so it can be required without transpiling
-const path = r
+const path = require('path')
+
+const normalizeId = (id) => {
+  if (!id) return id
+  if (id.length === 36) return id
+  if (id.length !== 32) {
+    thr
