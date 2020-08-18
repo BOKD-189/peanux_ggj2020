af46cb2f84a45b5c9a868c610d054d67dd6b6fa1
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-18 09:38:11 +0000

    Some code formatting

--- src/lib/notion/server-constants.js
+++ src/lib/notion/server-constants.js
@@ -16,4 +16,6 @@ const normalizeId = (id) => {
 }
 
 const NOTION_TOKEN = process.env.NOTION_TOKEN
-const BLOG_INDEX_ID = nor
+const BLOG_INDEX_ID = normalizeId(process.env.BLOG_INDEX_ID)
+const API_ENDPOINT = 'https://www.notion.so/api/v3'
+const BLOG_INDEX_CACHE = path.resolve('.blog_index_data
