60bae045df71c6722d956d695b6a1fc7a00c124e
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-18 11:27:00 +0000

    small mod

--- src/lib/notion/utils.ts
+++ src/lib/notion/utils.ts
@@ -1,3 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export fun
+export function setHeaders(req: NextApiRequest, res: NextApiResponse): boolean {
+  // set SPR/CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('
