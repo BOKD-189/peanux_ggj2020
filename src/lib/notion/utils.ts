dbf8ad892572bc7d6f9209b356b03108d427b7d9
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-18 12:55:13 +0000

    _automated commiting svg automated color system

--- src/lib/notion/utils.ts
+++ src/lib/notion/utils.ts
@@ -3,4 +3,6 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export function setHeaders(req: NextApiRequest, res: NextApiResponse): boolean {
   // set SPR/CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('
+  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
+  res.setHeader('Access-Control-Allow-Headers'
