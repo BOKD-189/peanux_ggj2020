1da2ff3689b2ffc1befd53d8db7ae693fd0e8e43
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-19 17:10:30 +0000

    sctp: update the netstamp_needed counter when copying sockets

--- src/pages/api/asset.ts
+++ src/pages/api/asset.ts
@@ -3,4 +3,11 @@ import getNotionAssetUrls from '../../lib/notion/getNotionAssetUrls'
 import { setHeaders, handleData, handleError } from '../../lib/notion/utils'
 
 export default async function notionApi(
-  req: NextA
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (setHeaders(req, res)) return
+  try {
+    const { assetUrl, blockId } = req.query as { [k: string]: string }
+
+    if (!asse
