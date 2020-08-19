d4e73387599d1fb5ff16982f2ee899c4b14b0c65
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-19 07:37:12 +0000

    Maven artifacts for The Knowledge Representation Interface version 1.1.6-SNAPSHOT

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Docume
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
