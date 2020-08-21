22327a41243a87ef9c000921f7de7f822bd2071f
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-21 22:15:35 +0000

    fix flake

--- src/pages/api/preview-post.ts
+++ src/pages/api/preview-post.ts
@@ -1,2 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import getPageData from 
+import getPageData from '../../lib/notion/getPageData'
+import getBlogIndex from '../../lib/notion/getBlogIndex'
+import { getBlogLink } from '../../lib/blog-helpers'
+
+export default async (req: NextApi
