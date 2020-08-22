619aef786c179b451eb35705750b0c92ca50f1fa
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-22 08:31:00 +0000

    Prepare 2.6.0 release

--- src/pages/api/preview-post.ts
+++ src/pages/api/preview-post.ts
@@ -3,4 +3,8 @@ import getPageData from '../../lib/notion/getPageData'
 import getBlogIndex from '../../lib/notion/getBlogIndex'
 import { getBlogLink } from '../../lib/blog-helpers'
 
-export default async (req: NextApi
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (typeof req.query.token !== 'string') {
+    return res.status(401).json({ message: 'invalid token' })
+  }
+  if (req.q
