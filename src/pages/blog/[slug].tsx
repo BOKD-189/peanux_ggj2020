907b3e179e20a5ae139469eaf55f883aa07149e6
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-24 09:04:48 +0000

    r8169: Config1 is read-only on 8168c and later.

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -38,3 +38,178 @@ export async function getStaticProps({ params: { slug }, preview }) {
   for (let i = 0; i < postData.blocks.length; i++) {
     const { value } = postData.blocks[i]
     const { type, properties } = value
+    if (type == 'tweet') {
+      const src = properties.source[0][0]
+      // parse id from https://twitter.com/_ijjk/status/TWEET_ID format
+      const tweetId = src.split('/')[5].split('?')[0]
+      if (!tweetId) continue
+
+      try {
+        const res = await fetch(
+          `https://api.twitter.com/1/statuses/oembed.json?id=${tweetId}`
+        )
+        const json = await res.json()
+        properties.html = json.html.split('<script')[0]
+        post.hasTweet = true
+      } catch (_) {
+        console.log(`Failed to get tweet embed for ${src}`)
+      }
+    }
+  }
+
+  const { users } = await getNotionUsers(post.Authors || [])
+  post.Authors = Object.keys(users).map((id) => users[id].full_name)
+
+  return {
+    props: {
+      post,
+      preview: preview || false,
+    },
+    revalidate: 10,
+  }
+}
+
+// Return our list of blog posts to prerender
+export async function getStaticPaths() {
+  const postsTable = await getBlogIndex()
+  // we fallback for any unpublished posts to save build time
+  // for actually published ones
+  return {
+    paths: Object.keys(postsTable)
+      .filter((post) => postsTable[post].Published === 'Yes')
+      .map((slug) => getBlogLink(slug)),
+    fallback: true,
+  }
+}
+
+const listTypes = new Set(['bulleted_list', 'numbered_list'])
+
+const RenderPost = ({ post, redirect, preview }) => {
+  const router = useRouter()
+
+  let listTagName: string | null = null
+  let listLastId: string | null = null
+  let listMap: {
+    [id: string]: {
+      key: string
+      isNested?: boolean
+      nested: string[]
+      children: React.ReactFragment
+    }
+  } = {}
+
+  useEffect(() => {
+    const twitterSrc = 'https://platform.twitter.com/widgets.js'
+    // make sure to initialize any new widgets loading on
+    // client navigation
+    if (post && post.hasTweet) {
+      if ((window as any)?.twttr?.widgets) {
+        ;(window as any).twttr.widgets.load()
+      } else if (!document.querySelector(`script[src="${twitterSrc}"]`)) {
+        const script = document.createElement('script')
+        script.async = true
+        script.src = twitterSrc
+        document.querySelector('body').appendChild(script)
+      }
+    }
+  }, [])
+  useEffect(() => {
+    if (redirect && !post) {
+      router.replace(redirect)
+    }
+  }, [redirect, post])
+
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
+  // if you don't have a post at this point, and are not
+  // loading one from fallback then  redirect back to the index
+  if (!post) {
+    return (
+      <div className={blogStyles.post}>
+        <p>
+          Woops! didn't find that post, redirecting you back to the blog index
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <Header titlePre={post.Page} />
+      {preview && (
+        <div className={blogStyles.previewAlertContainer}>
+          <div className={blogStyles.previewAlert}>
+            <b>Note:</b>
+            {` `}Viewing in preview mode{' '}
+            <Link href={`/api/clear-preview?slug=${post.Slug}`}>
+              <button className={blogStyles.escapePreview}>Exit Preview</button>
+            </Link>
+          </div>
+        </div>
+      )}
+      <div className={blogStyles.post}>
+        <h1>{post.Page || ''}</h1>
+        {post.Authors.length > 0 && (
+          <div className="authors">By: {post.Authors.join(' ')}</div>
+        )}
+        {post.Date && (
+          <div className="posted">Posted: {getDateStr(post.Date)}</div>
+        )}
+
+        <hr />
+
+        {(!post.content || post.content.length === 0) && (
+          <p>This post has no content</p>
+        )}
+
+        {(post.content || []).map((block, blockIdx) => {
+          const { value } = block
+          const { type, properties, id, parent_id } = value
+          const isLast = blockIdx === post.content.length - 1
+          const isList = listTypes.has(type)
+          let toRender = []
+
+          if (isList) {
+            listTagName = components[type === 'bulleted_list' ? 'ul' : 'ol']
+            listLastId = `list${id}`
+
+            listMap[id] = {
+              key: id,
+              nested: [],
+              children: textBlock(properties.title, true, id),
+            }
+
+            if (listMap[parent_id]) {
+              listMap[id].isNested = true
+              listMap[parent_id].nested.push(id)
+            }
+          }
+
+          if (listTagName && (isLast || !isList)) {
+            toRender.push(
+              React.createElement(
+                listTagName,
+                { key: listLastId! },
+                Object.keys(listMap).map((itemId) => {
+                  if (listMap[itemId].isNested) return null
+
+                  const createEl = (item) =>
+                    React.createElement(
+                      components.li || 'ul',
+                      { key: item.key },
+                      item.children,
+                      item.nested.length > 0
+                        ? React.createElement(
+                            components.ul || 'ul',
+                            { key: item + 'sub-list' },
+                            item.nested.map((nestedId) =>
+                              createEl(listMap[nestedId])
+                            )
+                          )
+                        : null
+                    )
+                  return createEl(listMap[itemId])
