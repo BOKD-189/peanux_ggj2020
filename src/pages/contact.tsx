8ffbba5dd28cc807361c5fb3bc6ee8ffa38f765f
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-25 12:00:11 +0000

    Update revisions.txt

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,8 @@
 import Header from '../components/header'
 import ExtLink from '../components/ext-link'
 
-import sharedStyles from '../styles/shared.m
+import sharedStyles from '../styles/shared.module.css'
+import contactStyles from '../styles/contact.module.css'
+
+import GitHub from '../components/svgs/github'
+import Twitter from '../components/sv
