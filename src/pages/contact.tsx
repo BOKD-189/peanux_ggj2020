de54d082c17321d4d16e267426d2e895d4577d7c
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-08-26 07:40:47 +0000

    added another example

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,4 +5,10 @@ import sharedStyles from '../styles/shared.module.css'
 import contactStyles from '../styles/contact.module.css'
 
 import GitHub from '../components/svgs/github'
-import Twitter from '../components/sv
+import Twitter from '../components/svgs/twitter'
+import Envelope from '../components/svgs/envelope'
+import LinkedIn from '../components/svgs/linkedin'
+
+const contacts = [
+  {
+    Comp: Twitt
