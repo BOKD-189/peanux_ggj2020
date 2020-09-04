af9a064b1d78b5b23acb6232d0bde923c6e33086
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-09-04 17:09:27 +0000

    Update prod_instance_with_bindle.md

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,4 +17,9 @@ export default function Index() {
         <h1>My Notion Blog</h1>
         <h2>
           Blazing Fast Notion Blog with Next.js'{' '}
-     
+          <ExtLink
+            href="https://github.com/vercel/next.js/issues/9524"
+            className="dotted"
+            style={{ color: 'inherit' }}
+          >
+       
