183184856118e466554e56fc560fff4d32f46bcf
BOKD-189 <104049690+BOKD-189@users.noreply.github.com>
2020-09-06 16:19:36 +0000

    Removed a TODO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,4 +31,6 @@ export default function Index() {
         <div className="explanation">
           <p>
             This is a statically generated{' '}
-            
+            <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
+            <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
+            is deployed wi
